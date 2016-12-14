5d1f2c822507ac7c343b3221f2a206f279bf1cd4
viktor <viktor@attendify.com>
2016-12-14 23:57:44 +0200

    day9

--- app/src/main/java/com/example/isakovv/adventofcode/Day9.kt
+++ app/src/main/java/com/example/isakovv/adventofcode/Day9.kt
@@ -11,9 +11,9 @@ object Advent17 {
         var markerEndIndex = 0
         var sequenceEndPos = 0
         var decodedSequence = ""
-        var marker: Marker? = null
+        var marker: Marker
         while (true) {
-            markerStartIndex = input8.indexOf('(', sequenceEndPos)
+            markerStartIndex = input9.indexOf('(', sequenceEndPos)
             if (markerStartIndex > sequenceEndPos) {
                 decodedSequence += input.substring(sequenceEndPos, markerStartIndex)
             }
@@ -21,21 +21,28 @@ object Advent17 {
                 decodedSequence += input.substring(sequenceEndPos, input.length)
                 break
             }
-
+            marker = Marker.parseMarker(input9, markerStartIndex)
+            markerEndIndex = markerStartIndex + marker.length + 1
+            decodedSequence += input9.substring(markerEndIndex, markerEndIndex + marker.repeatedSequenceLength).repeat(marker.repeatedCount)
+            sequenceEndPos = markerEndIndex + marker.repeatedSequenceLength
         }
-
+        return decodedSequence.length
     }
 }
 
 data class Marker(val length: Int, val repeatedSequenceLength: Int, val repeatedCount: Int) {
     companion object {
-        val pattern = Pattern.compile("((\\d+)x(\\d+))")
+        val pattern = Pattern.compile("\\((\\d+)x(\\d+)\\)")
         fun parseMarker(input: String, startPos: Int): Marker {
             val endPos = input.indexOf(')', startPos)
             if (endPos == -1)
                 throw IllegalStateException("Can't find marker end")
             val markerString = input.substring(startPos, endPos + 1)
-            return Marker(endPos - startPos, Integer.valueOf(pattern.matcher(markerString).group(1)), Integer.valueOf(pattern.matcher(markerString).group(2)))
-        }
+            val matcher = pattern.matcher(markerString)
+            if (matcher.find()) {
+                return Marker(endPos - startPos, Integer.valueOf(matcher.group(1)), Integer.valueOf(matcher.group(2)))
+            }
+            throw IllegalStateException("Can't find marker. startPos = $startPos")
+            }
     }
 }
